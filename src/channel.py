3c03579ed96edbe455f5487133795d350d2d610d
Mikhail Gribkov <mishacool4@gmail.com>
2023-07-12 13:21:22 +0300

    done homework 2

--- src/channel.py
+++ src/channel.py
@@ -3,17 +3,40 @@ import os
 from googleapiclient.discovery import build
 
 api_key: str = os.getenv('YT_API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-        self.channel = youtube.channels().list(id=self.channel_id, part='snippet, statistics').execute()
+        self.__channel_id = channel_id
+        self.channel = Channel.youtube.channels().list(id=self.__channel_id, part='snippet, statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/channel/' + self.__channel_id
+        self.subscribers = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    def to_json(self, name):
+        json_str = json.dumps({'id': self.__channel_id, 'Title': self.title,
+                               'Description': self.description, 'URL': self.url,
+                               'Subscribers': self.subscribers,
+                               'Video count': self.video_count,
+                               'View count': self.view_count}, indent=2, ensure_ascii=False)
+        with open(name, 'w', encoding='utf-8') as file:
+            file.write(json_str)
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
