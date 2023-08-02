ee0cc12d60050b1d6abad19dad38e3aa93ff0919
Mikhail Gribkov <mishacool4@gmail.com>
2023-08-02 17:37:13 +0300

    done homework 6

--- src/video.py
+++ src/video.py
@@ -11,13 +11,23 @@ class Video:
     def __init__(self, video_id: str) -> None:
         """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
         self.__video_id = video_id
-        self.video = Video.youtube.videos().list(id=self.__video_id, part='snippet, statistics, contentDetails').execute()
-        self.title = self.video['items'][0]['snippet']['title']
-        self.url = 'https://www.youtube.com/watch?v=' + self.__video_id
-        self.like_count = self.video['items'][0]['statistics']['likeCount']
-        self.view_count = self.video['items'][0]['statistics']['viewCount']
-        self.duration = self.video['items'][0]['contentDetails']['duration']
-        self.url_short = 'https://youtu.be/' + self.__video_id
+        try:
+            self.video = Video.youtube.videos().list(id=self.__video_id,
+                                                     part='snippet, statistics, contentDetails').execute()
+            self.title = self.video['items'][0]['snippet']['title']
+            self.url = 'https://www.youtube.com/watch?v=' + self.__video_id
+            self.like_count = self.video['items'][0]['statistics']['likeCount']
+            self.view_count = self.video['items'][0]['statistics']['viewCount']
+            self.duration = self.video['items'][0]['contentDetails']['duration']
+            self.url_short = 'https://youtu.be/' + self.__video_id
+        except:
+            self.video = None
+            self.title = None
+            self.url = None
+            self.like_count = None
+            self.view_count = None
+            self.duration = None
+            self.url_short = None
 
     def __str__(self):
         return f'{self.title}'
